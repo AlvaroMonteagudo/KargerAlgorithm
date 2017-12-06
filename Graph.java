d1d66f56c19070c40a9c3f85cb117bdb6be23f26
siluf <681721@unizar.es>
2017-12-06 14:57:07 +0100

    Metodos necesarios que hay que realizar con la estructura

--- Graph.java
+++ Graph.java
@@ -1,6 +1,20 @@
 public class Graph {
 
-	public Graph() {}
+	public Graph() {
+		
+	}
+	/*
+	 * Add the Vertex <v> to the Graph
+	 */
+	public void addVertex( Vertex v ){
+		
+    }
+	/*
+	 * Return one Vertex of the Graph
+	 */
+    public Vertex getVertex( ) {
+    	
+    }
 
 
 }
